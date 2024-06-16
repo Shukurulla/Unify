4a8f1a43ed84661143cb45fd84b552467930f7d9
Ernazar <kuanishbayevyernazar@gmail.com>
2024-06-16 12:11:55 +0500

    Product price bugs fixed.

--- src/utilities/numberFormat.js
+++ src/utilities/numberFormat.js
@@ -1,10 +1,14 @@
 export function formatCurrency(number) {
+    const divider = 1000
+    const arr = []
+
     while (number > divider) {
         if (number % divider === 0) {
             arr.push('000')
         } else {
             arr.push(number % divider)
         }
+        
         number -= number % divider
         number /= divider
     }
