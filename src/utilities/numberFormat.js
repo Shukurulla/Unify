24096137349f519fe97c98d0694efd90feaf2004
Ernazar <kuanishbayevyernazar@gmail.com>
2024-06-16 12:40:10 +0500

    numberFormat.js file bugs fixed.

--- src/utilities/numberFormat.js
+++ src/utilities/numberFormat.js
@@ -1,17 +1,21 @@
 export function formatCurrency(number) {
-    const divider = 1000
-    const arr = []
-
-    while (number > divider) {
-        if (number % divider === 0) {
-            arr.push('000')
-        } else {
-            arr.push(number % divider)
+    if (number > 0) {
+        const divider = 1000
+        const arr = []
+    
+        while (number > divider) {
+            if (number % divider === 0) {
+                arr.push('000')
+            } else {
+                arr.push(number % divider)
+            }
+            
+            number -= number % divider
+            number /= divider
         }
         
-        number -= number % divider
-        number /= divider
+        return number + '.' + arr.join('.')
+    } else {
+        return 0
     }
-    
-    return number + '.' + arr.join('.')
 }
