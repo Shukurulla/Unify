3dc472d270bd99b253f3b7d5168baa14c86b7c4d
Ernazar <kuanishbayevyernazar@gmail.com>
2024-06-26 14:17:09 +0500

    Floating button style has been changed.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,7 @@ import { IoIosNotifications } from "react-icons/io"
 import { GoPlus } from "react-icons/go"
 import HeroImg from '../assets/hero-image.png'
 import DiscountImg from '../assets/discount.png'
+import { FaBellConcierge, FaCartShopping } from "react-icons/fa6"
 
 const Home = () => {
   const [clickedSortingItemIndex, setClickedSortingItemIndex] = useState(0)
@@ -43,8 +44,11 @@ const Home = () => {
     <div className='bg-gradient-to-r from-[#8CD23C] to-[#417A00]'>
       <Navbar />
       <div className="z-10 fixed bottom-10 left-1/2 -translate-x-1/2">
-        <button onClick={() => setFloatingButton(prev => !prev)} className={`shadow-[0_0_12px_0_#77AC63] size-10 text-white flex justify-center items-center rounded-full bg-gradient-to-r from-[#8CD23C] to-[#417A00] backdrop-blur-md ease-in-out duration-300 ${floatingButtonIsOpen && 'scale-150 shadow-xl'}`}>
-          <GoPlus className={`mx-auto text-2xl stroke-1 duration-300 ease ${floatingButtonIsOpen && 'rotate-45'}`} />
+        <button onClick={() => setFloatingButton(prev => !prev)} className={`relative shadow-[0_0_12px_0_#77AC63] size-14 text-white flex justify-center items-center rounded-full bg-gradient-to-r from-[#8CD23C] to-[#417A00] backdrop-blur-md ease-in-out duration-300 ${floatingButtonIsOpen && 'scale-150 shadow-xl'}`}>
+          {/* <GoPlus className={`mx-auto text-2xl stroke-1 duration-300 ease ${floatingButtonIsOpen && 'rotate-45'}`} /> */}
+          <FaCartShopping className="absolute bottom-[45%] right-1/2" />
+          <div className="w-[2px] h-8 bg-white rounded-full rotate-45"></div>
+          <FaBellConcierge className="absolute top-[45%] left-1/2" />
         </button>
         <Link ref={waitersRef} to='waiters' className={`absolute bottom-40 left-1/2 -translate-x-1/2 transition ease duration-300 delay-150 opacity-0 ${floatingButtonIsOpen ? 'scale-100 opacity-100' : 'scale-50'}`} >
           <div className='shadow-xl border-2 border-white size-16 text-white flex justify-center items-center rounded-full bg-gradient-to-r from-[#8CD23C] to-[#417A00] backdrop-blur-md'>
